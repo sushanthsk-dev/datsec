63486a8de412f8452bdaf7739faa984d8b37a895
Sushanth Sk <email>
2021-02-16 21:36:01 +0530

    Updated

---
+++ hello.js
@@ -0,0 +1,12 @@
+let data = [
+  {
+    stepsDescription: 'Hello worldd',
+    stepsImg: 'Hello world',
+  },
+];
+
+data.push({
+  stepsDescription: 'Hello worldd',
+  stepsImg: 'Hello world',
+});
+console.log(data);
